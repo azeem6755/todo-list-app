39f4603aba63b7152aeb67f8f04c415594abd04c
Azeem Shaikh <azeem6755@gmail.com>
2025-11-01 12:57:30 +0530

    ADD: root_path to project

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@ from .config import get_settings
 from .router import user, auth, task
 
 
-app = FastAPI(title=get_settings().APP_NAME)
+app = FastAPI(title=get_settings().APP_NAME, root_path='/todo/')
 
 app.include_router(user.router)
 app.include_router(auth.router)
